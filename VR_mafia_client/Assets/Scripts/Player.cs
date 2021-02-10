de7db32c49c04607bd8bb6053bef41d659243084
irikong <gookirix@gmail.com>
2021-02-10 20:42:24 +0900

    플레이어 움직임 수정

--- VR_mafia_client/Assets/Scripts/Player.cs
+++ VR_mafia_client/Assets/Scripts/Player.cs
@@ -7,13 +7,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController CC;
+    private Transform HEAD;
+    private Transform BODY;
     private Vector3 moveDirection = Vector3.zero;
 
     [Header("속도 및 중력")]
     public float moveSpeed = 4.0F;
     public float jumpSpeed = 8.0F;
-    public float rotateSpeed = 2.0F;
+    public float rotateSpeed = 100.0F;
     public float gravity = 20.0F;
+
+    private float rotX, rotY;
 
     void Start()
     {
@@ -22,20 +26,24 @@ public class Player : MonoBehaviour
         {
             Debug.Log("CC is empty..!");
         }
+        HEAD = transform.GetChild(0);
+        BODY = transform.GetChild(1);
+
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        Move();
         Rotate();
+        Move();
     }
 
+    #region 움직임 관련
     void Move()
     {
         if (CC.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= moveSpeed;
             if (Input.GetButton("Jump"))
@@ -48,6 +56,16 @@ public class Player : MonoBehaviour
     }
     void Rotate()
     {
-        transform.Rotate(0f, Input.GetAxis("Mouse X") * rotateSpeed, 0f);
+        rotX += Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+        rotY += Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+
+        if (rotX <= -90f) rotX = -90f;
+        else if (90f <= rotX) rotX = 90f;
+        //if (90f <= rotY) rotY = 90f;
+        //if (rotY <= -90f) rotY = -90f;
+
+        transform.eulerAngles = new Vector3(0f, rotY, 0);
+        HEAD.transform.eulerAngles = new Vector3(-rotX, rotY, 0f);
     }
+    #endregion
 }
