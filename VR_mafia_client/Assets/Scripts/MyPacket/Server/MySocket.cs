a8d0922ae314324ac3ce7e1ce56c168044610587
irikong <gookirix@gmail.com>
2021-02-17 22:06:36 +0900

    MyPacket 추가, TCP Client 수정

---
+++ VR_mafia_client/Assets/Scripts/MyPacket/Server/MySocket.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MyPacket
+{
+    class MySocket
+    {
+        #region delegate
+        public delegate void MessageHandler(MySocket socket, Packet packet);
+        #endregion
+        #region field
+        protected TcpClient client;
+        protected NetworkStream stream;
+        protected PacketHeader header;
+        protected byte[] buffer;
+        protected Dictionary<PacketType, MessageHandler> handler;
+        #endregion
+        #region constructor
+        public MySocket(TcpClient client)
+        {
+            this.client = client;
+            stream = client.GetStream();
+            buffer = new byte[1024];
+            HandlerInit();
+        }
+        #endregion
+        #region public method
+        /// <summary>
+        /// 클라이언트와 연결 해제
+        /// </summary>
+        public void Disconnect()
+        {
+            Emit(PacketType.DISCONNECT);
+            Close();
+        }
+        /// <summary>
+        /// 소켓 연결 해제
+        /// </summary>
+        public void Close()
+        {
+            stream.Close();
+            client.Close();
+        }
+        /// <summary>
+        /// 이벤트 핸들러 등록
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="handler"></param>
+        public void On(PacketType type, MessageHandler handler)
+        {
+            this.handler[type] += handler;
+        }
+        /// <summary>
+        /// 특정 종류 패킷의 핸들러 제거
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="handler"></param>
+        public void Off(PacketType type, MessageHandler handler)
+        {
+            this.handler[type] -= handler;
+        }
+        /// <summary>
+        /// 특정 종류 패킷의 핸들러 초기화
+        /// </summary>
+        /// <param name="type"></param>
+        public void Clear(PacketType type)
+        {
+            this.handler[type] = EmptyHandler;
+        }
+        /// <summary>
+        /// 특정 종류의 패킷과 해당하는 데이터를 소켓에 전송
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="data"></param>
+        public void Emit(PacketType type, byte[] bytes = null)
+        {
+            var packet = new Packet(type, bytes);
+            lock (stream)
+            {
+                stream.Write(packet.ToBytes(), 0, packet.Size);
+            }
+        }
+        /// <summary>
+        /// 비동기 방식으로 메시지 처리 시작
+        /// </summary>
+        public virtual void Listen()
+        {
+            var thread = new Thread(new ThreadStart(ListenMessage));
+            thread.Start();
+        }
+        #endregion
+        #region private method
+        /// <summary>
+        /// 모든 패킷 종류에 대한 핸들러를 공백 핸들러로 초기화
+        /// </summary>
+        private void HandlerInit()
+        {
+            handler = new Dictionary<PacketType, MessageHandler>();
+            foreach (PacketType type in Enum.GetValues(typeof(PacketType)))
+            {
+                handler[type] = EmptyHandler;
+            }
+        }
+        /// <summary>
+        /// 클라이언트가 연결되어 있는 동안 패킷을 읽고 처리
+        /// </summary>
+        private void ListenMessage()
+        {
+            while (client.Connected)
+            {
+                if (stream.CanRead && stream.DataAvailable)
+                {
+                    ReadPacket();
+                    handler[header.Type](this, new Packet(header, buffer));
+                }
+            }
+        }
+        /// <summary>
+        /// 스트림에서 패킷을 읽고 버퍼에 저장
+        /// </summary>
+        private void ReadPacket()
+        {
+            lock (stream)
+            {
+                stream.Read(buffer, 0, PacketHeader.SIZE);
+                header.FromBytes(buffer);
+                if (buffer.Length < header.Size)
+                    buffer = new byte[buffer.Length * 2];
+                if (header.Size != 0)
+                    stream.Read(buffer, 0, header.Size);
+            }
+        }
+        /// <summary>
+        /// 공백 핸들러
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <param name="data"></param>
+        /// 
+        private void EmptyHandler(MySocket socket, Packet packet) { }
+        #endregion
+    }
+}
--- VR_mafia_client/Assets/Scripts/TestClientManager.cs
+++ VR_mafia_client/Assets/Scripts/TestClientManager.cs
@@ -6,25 +6,12 @@ using System.IO;
 using System.Text;
 using System.Net.Sockets;
 using System.Threading;
+using MyPacket;
 
 public class TestClientManager : MonoBehaviour
 {
-	[Serializable]
-	enum PacketType
-	{
-		NONE,
-		CONNECT,
-		DISCONNECT,
-		SET_NAME,
-		ROOM_LIST_REQ,
-		MOVE,
-		END
-	}
-
-	private TcpClient socket;
-	private NetworkStream stream;
-	private StreamWriter writer;
-	private StreamReader reader;
+	private TcpClient client;
+	private MySocket socket;
 	private bool socketReady;
 
 	public string hostIp;
@@ -37,16 +24,16 @@ public class TestClientManager : MonoBehaviour
     }
     private void Update()
     {
-        if(socketReady && stream.DataAvailable)
+        if (socketReady)
         {
-			string data = reader.ReadLine();
-			if(data != null)
-            {
-				Debug.Log(data);
-				Send("받았음.");
-            }
+
         }
     }
+	private void OnApplicationQuit()
+	{
+		CloseSocket();
+	}
+
 	public void ConnectToServer()
 	{
 		if (socketReady) return;
@@ -56,46 +43,51 @@ public class TestClientManager : MonoBehaviour
 
 		try
 		{
-			socket = new TcpClient(hostIp, port);
-			stream = socket.GetStream();
-			writer = new StreamWriter(stream);
-			reader = new StreamReader(stream);
+			client = new TcpClient(hostIp, port);
+			
+			socket = new MySocket(client);
+			socket.On(PacketType.CONNECT, OnConnect);
+			socket.On(PacketType.DISCONNECT, OnDisconnect);
+			socket.On(PacketType.SET_NAME, OnSetName);
+
+			socket.Listen();
+			socket.Emit(PacketType.CONNECT);
 			socketReady = true;
-			Emit();
 		}
 		catch (Exception e)
 		{
 			Debug.Log("Socket Error" + e.Message);
 		}
 	}
-	void Send(string data)
+
+	private void OnConnect(MySocket socket, Packet packet)
+	{
+		//var data = new ConnectData();
+		//data.FromBytes(packet.Bytes);
+		Debug.Log("OnConnect");
+		//PlayerID = data.player_id;
+	}
+	private void OnDisconnect(MySocket socket, Packet packet)
+	{
+		socket.Disconnect();
+		Debug.Log("Disconnect");
+	}
+	private void OnSetName(MySocket socket, Packet packet)
+	{
+		//var data = new SetNameData();
+		//data.FromBytes(packet.Bytes);
+		Debug.Log("OnSetName");
+        //UserName = data.UserName;
+        //Dispatcher.Invoke(() => { userNameInput.Text = UserName; });
+    }
+
+	private void CloseSocket()
     {
 		if (!socketReady) return;
 
-		writer.WriteLine(data);
-		writer.Flush();
-    }
+		socket.Emit(PacketType.DISCONNECT);
+		socket.Disconnect();
 
-    private void OnApplicationQuit()
-    {
-		CloseSocket();
-    }
-	void CloseSocket()
-    {
-		if (!socketReady) return;
-
-		writer.Close();
-		reader.Close();
-		socket.Close();
 		socketReady = false;
     }
-
-	public void Emit()
-	{
-		var result = new byte[8];
-		BitConverter.GetBytes(2).CopyTo(result, 0);
-		BitConverter.GetBytes(0).CopyTo(result, 4);
-
-		stream.Write(result, 0, 8);
-	}
 }
