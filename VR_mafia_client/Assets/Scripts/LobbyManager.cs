bb92743ef757dcf964e97266990d8e4e0c5629df
irikong <gookirix@gmail.com>
2021-03-09 00:05:55 +0900

    RoomList 추가

---
+++ VR_mafia_client/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviour
+{
+    [SerializeField] private GameObject RoomList;
+
+    [Header("Button")]
+    [SerializeField] private Button JoinButton;
+    [SerializeField] private Button CreateButton;
+    [SerializeField] private Button RefreshButton;
+
+    void Start()
+    {
+        CreateButton.onClick.AddListener(OnCreateButton);
+    }
+
+    void OnCreateButton()
+    {
+
+    }
+}
