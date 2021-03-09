f5f2c6efd7fde75b2f7aaf25bcef1fd2375b255d
irikong <gookirix@gmail.com>
2021-03-09 21:59:00 +0900

    LobyManager 수정

--- VR_mafia_client/Assets/Scripts/LobbyManager.cs
+++ VR_mafia_client/Assets/Scripts/LobbyManager.cs
@@ -6,15 +6,26 @@ using UnityEngine.UI;
 public class LobbyManager : MonoBehaviour
 {
     [SerializeField] private GameObject roomList;
+    [Header("UI")]
+    [SerializeField] private Text playerName;
 
     [Header("Button")]
     [SerializeField] private Button joinButton;
     [SerializeField] private Button createButton;
     [SerializeField] private Button refreshButton;
+    [SerializeField] private Button changeNameButton;
+
+    [Header("ChangeNamePanel")]
+    [SerializeField] private GameObject changeNamePanel;
+    [SerializeField] private InputField inputField;
+    [SerializeField] private Button okButton;
+    [SerializeField] private Button noButton;
 
     void Start()
     {
         createButton.onClick.AddListener(OnCreateButton);
+
+        InitChangeNamePanel();
     }
 
     void OnJoinButton()
@@ -31,4 +42,33 @@ public class LobbyManager : MonoBehaviour
     {
 
     }
+
+    #region ChangeNamePanel
+    void InitChangeNamePanel()
+    {
+        changeNamePanel.SetActive(true);
+
+        changeNameButton.onClick.AddListener(OnChangeNameButton);
+        okButton.onClick.AddListener(OnOKButton);
+        noButton.onClick.AddListener(OnNoButton);
+
+        inputField.characterLimit = 10;
+
+        changeNamePanel.SetActive(false);
+    }
+    void OnChangeNameButton()
+    {
+        changeNamePanel.SetActive(true);
+        inputField.text = playerName.text;
+    }
+    void OnOKButton()
+    {
+        playerName.text = inputField.text;
+        changeNamePanel.SetActive(false);
+    }
+    void OnNoButton()
+    {
+        changeNamePanel.SetActive(false);
+    }
+    #endregion
 }
