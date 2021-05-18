cbf2aecbd7ceaaef7c79ff61bf3e6327d179fa38
DESKTOP-C5LVN2Q\241 <wjd5126@koreatech.ac.kr>
2021-05-18 19:45:48 +0900

    패킷 데이터 코드 추가

---
+++ TcpServer/TcpServer/MyPacket/PacketData/InGame/FinalVoteResData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPacket
+{
+    public class FinalVoteResData : IPacketData
+    {
+        public int Result;
+        public int Size
+        {
+            get
+            {
+                return 4;
+            }
+        }
+
+        public FinalVoteResData()
+        {
+            Result = 0;
+
+        }
+
+
+        public FinalVoteResData(int result)
+        {
+            Result = result;
+  
+        }
+
+        public void FromBytes(byte[] bytes)
+        {
+            Result = BitConverter.ToInt32(bytes, 0);
+        }
+
+        public byte[] ToBytes()
+        {
+            return BitConverter.GetBytes(Result);
+        }
+    }
+}
---
+++ TcpServer/TcpServer/MyPacket/PacketData/InGame/VoteResData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPacket
+{
+    public class VoteResData : IPacketData
+    {
+        public int Result;
+        public int Size
+        {
+            get
+            {
+                return 4;
+            }
+        }
+
+        public VoteResData()
+        {
+            Result = 0;
+
+        }
+
+
+        public VoteResData(int result)
+        {
+            Result = result;
+  
+        }
+
+        public void FromBytes(byte[] bytes)
+        {
+            Result = BitConverter.ToInt32(bytes, 0);
+        }
+
+        public byte[] ToBytes()
+        {
+            return BitConverter.GetBytes(Result);
+        }
+    }
+}
