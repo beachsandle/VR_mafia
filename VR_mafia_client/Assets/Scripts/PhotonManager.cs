6489a5c5c832d91648696cff9485fed8ef9e9b00
YunSangun <tkaltk123@gmail.com>
2021-06-29 15:43:24 +0900

    포톤 매니저 추가

---
+++ VR_mafia_client/Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    #region singleton
+    private static PhotonManager instance;
+    public static PhotonManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<PhotonManager>();
+                DontDestroyOnLoad(instance.gameObject);
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    public void JoinRoom(int roomId)
+    {
+        PhotonNetwork.JoinOrCreateRoom(roomId.ToString(), null, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Join room success");
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Join Room fail");
+        Application.Quit();
+    }
+
+}
