a400a47577d3919068ea6026e30a4df139a3b235
YunSangun <tkaltk123@gmail.com>
2021-08-23 11:53:45 +0900

    프로젝트 복제

---
+++ VR_MAFIA_CLIENT_VR/Assets/Scripts/Ingame/VotingPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VotingPanel : MonoBehaviour
+{
+    private void OnDisable()
+    {
+        var votingContent = transform.GetChild(0);
+
+        foreach (var btn in votingContent.GetComponentsInChildren<Button>())
+        {
+            btn.transform.Find("Text").GetComponent<Text>().color = Color.white;
+            btn.transform.Find("Count Text").GetComponent<Text>().text = "0";
+            btn.transform.Find("Selected UI").gameObject.SetActive(false);
+        }
+    }
+}
