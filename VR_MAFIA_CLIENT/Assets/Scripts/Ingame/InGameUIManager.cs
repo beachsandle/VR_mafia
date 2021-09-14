93624cff7dc86e5c0068ad02845f3e3825f5ae04
irikong <gookirix@gmail.com>
2021-09-14 21:55:07 +0900

    Menu 입력 추가

--- VR_MAFIA_CLIENT/Assets/Scripts/Ingame/InGameUIManager.cs
+++ VR_MAFIA_CLIENT/Assets/Scripts/Ingame/InGameUIManager.cs
@@ -48,7 +48,7 @@ public class InGameUIManager : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (isVR ? OVRInput.GetDown(OVRInput.Button.Start) : Input.GetKeyDown(KeyCode.Escape))
             OnMenuButton();
     }
     private void OnDestroy()
