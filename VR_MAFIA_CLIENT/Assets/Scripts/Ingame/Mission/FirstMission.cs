376c684b6b7e8b0a6e348972ed8c2fa7dc83aab0
irikong <gookirix@gmail.com>
2021-11-16 22:20:34 +0900

    FirstMission 플레이 가능하도록 변경

--- VR_MAFIA_CLIENT/Assets/Scripts/Ingame/Mission/FirstMission.cs
+++ VR_MAFIA_CLIENT/Assets/Scripts/Ingame/Mission/FirstMission.cs
@@ -2,20 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FirstMission : MonoBehaviour
 {
-    private int num = 4;
+    const int DIGIT = 4;
     private int password;
+    private int inputNum;
+    private int buttonLeft = DIGIT;
+    private Button[] buttons;
 
-    void Start()
+    private void Awake()
     {
+        FindReference();
         InitPassword();
+        InitButtonEvent();
+    }
+
+    private void OnDisable()
+    {
+        for(int i = 0; i < DIGIT; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+
+    private void FindReference()
+    {
+        buttons = GetComponentsInChildren<Button>();
+    }
+
+    private void InitButtonEvent()
+    {
+        for(int i = 0; i < DIGIT; i++)
+        {
+            int num = i + 1;
+            buttons[i].onClick.AddListener(() =>
+            {
+                OnNumButton(num);
+            });
+        }
     }
 
     private void InitPassword()
     {
-        List<int> list = Enumerable.Range(1, num).ToList();
+        List<int> list = Enumerable.Range(1, DIGIT).ToList();
         int place = 1000;
         while(0 < list.Count)
         {
@@ -24,10 +55,35 @@ public class FirstMission : MonoBehaviour
             list.RemoveAt(idx);
             place /= 10;
         }
+
+        Debug.Log("password : " + password);
+    }
+
+    private void OnNumButton(int n)
+    {
+        buttonLeft--;
+
+        inputNum += (int)Mathf.Pow(10, buttonLeft) * n;
+        buttons[n - 1].interactable = false;
+
+        if(buttonLeft == 0)
+        {
+            MissionFinish();
+        }
     }
 
     public void MissionStart()
     {
+        inputNum = 0;
+        buttonLeft = 4;
         gameObject.SetActive(true);
+    }
+
+    private void MissionFinish()
+    {
+        bool isSuccess = (password == inputNum);
+        Debug.Log("Mission : " + (isSuccess ? "Clear" : "Fail"));
+
+        gameObject.SetActive(false);
     }
 }
